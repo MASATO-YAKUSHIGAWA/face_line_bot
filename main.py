0b723e8a4f1fd68b71331bd57edd3f8a2889a031
MASATO-YAKUSHIGAWA <m.yks.022@gmail.com>
2019-11-01 22:30:44 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,65 @@
+from flask import Flask, request, abort
+import os
+
+import face_detect as f  # face_detect.py
+import base64
+
+from linebot import (
+   LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+   InvalidSignatureError
+)
+from linebot.models import (
+   MessageEvent, TextMessage, TextSendMessage, ImageMessage #ImageMessageを追加
+)
+
+app = Flask(__name__)
+
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ['YOUR_CHANNEL_ACCESS_TOKEN']
+YOUR_CHANNEL_SECRET = os.environ['YOUR_CHANNEL_SECRET']
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+   # get X-Line-Signature header value
+   signature = request.headers['X-Line-Signature']
+
+   # get request body as text
+   body = request.get_data(as_text=True)
+   app.logger.info("Request body: " + body)
+
+   # handle webhook body
+   try:
+       handler.handle(body, signature)
+   except InvalidSignatureError:
+       abort(400)
+
+   return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage) #テキストの場合
+# テキストの場合はオウム返し
+def handle_message(event):
+   line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+
+@handler.add(MessageEvent, message=ImageMessage) #画像の場合
+def handle_image_message(event):
+   push_img_id = event.message.id # 投稿された画像IDを取得
+   message_content = line_bot_api.get_message_content(push_img_id) # LINEサーバー上に自動保存された画像を取得
+   push_img = b"" #b""にしているのは、画像代入のため（バイト型データを扱えるように）
+   for chunk in message_content.iter_content(): 
+       push_img += chunk #画像をiter_content（画像を分割）でpush_imgに順次代入
+   push_img = base64.b64encode(push_img) # APIに通すためbase64エンコード
+   msg = f.face_detect(push_img) #face_detect呼び出し
+   line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
+
+if __name__ == "__main__":
+   #    app.run()
+   port = int(os.getenv("PORT"))
+   app.run(host="0.0.0.0", port=port)
